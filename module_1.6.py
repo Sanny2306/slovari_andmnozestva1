0bf6c2b3852c93045c6771724a0a04807d7abaa1
Sanny2306 <rastegaev.90@bk.ru>
2024-09-07 08:05:26 +0300

    не понял на счет пары с ключем,

--- module_1.6.py
+++ module_1.6.py
@@ -1,18 +1,17 @@
-#my_dict = {"Alex":1990, "Kris":1995, "Bony":2014}
-#print(my_dict)
-#print(my_dict["Alex"])
-#my_dict["Genry"]= 1977
-#print(my_dict)
-#my_dict.update({"Leonid":1980,
-                #"Olia":1987})
-#print(my_dict)
-#del my_dict["Genry"]
-#print(my_dict)
-#print(my_dict.get("Genry"))
-
+my_dict = {"Alex":1990, "Kris":1995, "Bony":2014}
+print(my_dict)
+print(my_dict["Alex"])
+my_dict["Genry"]= 1977
+print(my_dict)
+my_dict.update({"Leonid":1980,
+                "Olia":1987})
+print(my_dict)
+del my_dict["Genry"]
+print(my_dict)
+print(my_dict.get("Genry"))
 my_set= {1,"alex",123.4,1,"alex",123.4}
 print(my_set)
-my_set={1,"alex",123.4,"alex",123.4,"avto",12}
+my_set=({1,"alex",123.4,"alex",123.4,"avto",12})
 print(my_set)
 print(my_set.discard("alex"))
 print(my_set)
